670c810bc0da6f757a634ea0c786a07a52682b93
Emil Koutanov <ekoutanov@gmail.com>
2025-06-05 11:35:15 +1000

    JSON parsing

--- tests/json_lex_parse.rs
+++ tests/json_lex_parse.rs
@@ -1,11 +1,11 @@
-use std::io::BufReader;
-use hg::{lexer, phrase, verse};
 use hg::lexer::tokenise;
 use hg::parser::parse;
 use hg::token::Token;
 use hg::token::Token::{Boolean, Dash, Decimal, Ident, Integer, Text};
 use hg::tree::Node::{Cons, List, Prefix, Raw};
 use hg::tree::{Node, Verse};
+use hg::{phrase, verse};
+use std::io::BufReader;
 
 fn tok_ok(str: &str) -> Vec<Token> {
     tokenise(BufReader::with_capacity(10, str.as_bytes())).unwrap().into()
@@ -19,6 +19,26 @@ fn string(value: &str) -> Node {
     Raw(Text(value.into()))
 }
 
+fn integer(value: u128) -> Node {
+    Raw(Integer(value))
+}
+
+fn decimal(whole: u128, fractional: u128, scale: u8) -> Node {
+    Raw(Decimal(whole, fractional, scale))
+}
+
+fn boolean(value: bool) -> Node {
+    Raw(Boolean(value))
+}
+
+fn null() -> Node {
+    Raw(Ident("null".into()))
+}
+
+fn negative(value: impl Into<Node>) -> Node {
+    Prefix(Dash, Box::new(value.into()))
+}
+
 fn key_value(key: &str, value: Node) -> Node {
     Cons(
         Box::new(Raw(Text(key.into()))),
@@ -26,8 +46,61 @@ fn key_value(key: &str, value: Node) -> Node {
     )
 }
 
+struct ArrayBuilder(Vec<Node>);
+
+impl ArrayBuilder {
+    fn with(mut self, element: impl Into<Node>) -> Self {
+        self.0.push(element.into());
+        self
+    }
+}
+
+impl From<ArrayBuilder> for Node {
+    fn from(array_builder: ArrayBuilder) -> Self {
+        let verses = array_builder.0.into_iter().map(|element| verse![phrase![element]]).collect();
+        List(verses)
+    }
+}
+
+fn array() -> ArrayBuilder {
+    ArrayBuilder(vec![])
+}
+
+struct ObjectBuilder(Vec<(&'static str, Node)>);
+
+impl ObjectBuilder {
+    fn key(self, key: &'static str) -> ObjectKeyValueBuilder {
+        ObjectKeyValueBuilder(self, key)
+    }
+}
+
+impl From<ObjectBuilder> for Node {
+    fn from(object_builder: ObjectBuilder) -> Self {
+        let verses = object_builder.0.into_iter().map(|(key, value)| verse![phrase![key_value(key, value)]]).collect();
+        List(verses)
+    }
+}
+
+fn object() -> ObjectBuilder {
+    ObjectBuilder(vec![])
+}
+
+struct ObjectKeyValueBuilder(ObjectBuilder, &'static str);
+
+impl ObjectKeyValueBuilder {
+    fn value(self, value: impl Into<Node>) -> ObjectBuilder {
+        let (mut object_builder, key) = (self.0, self.1);
+        object_builder.0.push((key, value.into()));
+        object_builder
+    }
+}
+
+fn root(node: impl Into<Node>) -> Verse {
+    verse![phrase![node.into()]]
+}
+
 #[test]
-fn small_json() {
+fn multilevel_json() {
     let str = r#"{
         "key1": "value1",
         "key2": 1234,
@@ -50,177 +123,32 @@ fn small_json() {
     }"#;
     let tokens = tok_ok(str);
     let verse = parse_ok(tokens);
-    assert_eq!(verse![
-        phrase![
-            List(vec![
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("key1".into()))), 
-                            phrase![Raw(Text("value1".into()))]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("key2".into()))), 
-                            phrase![Raw(Integer(1234))]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("key3".into()))), 
-                            phrase![Raw(Decimal(1234, 5678, 4))]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("key4".into()))), 
-                            phrase![Prefix(Dash, Box::new(Raw(Integer(345))))]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("key5".into()))), 
-                            phrase![Raw(Boolean(true))]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("key6".into()))), 
-                            phrase![Raw(Ident("null".into()))]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("emptyArray".into()))), 
-                            phrase![List(vec![])]
-                        )
-                    ]
-                ],
-                verse![
-                    phrase![
-                        Cons(
-                            Box::new(Raw(Text("employees".into()))), 
-                            phrase![List(vec![
-                                verse![
-                                    phrase![
-                                        List(
-                                            vec![
-                                                verse![
-                                                    phrase![
-                                                        Cons(
-                                                            Box::new(Raw(Text("id".into()))), 
-                                                            phrase![Raw(Integer(1))]
-                                                        )
-                                                    ]
-                                                ],
-                                                verse![
-                                                    phrase![
-                                                        Cons(
-                                                            Box::new(Raw(Text("details".into()))), 
-                                                            phrase![
-                                                                List(vec![
-                                                                    verse![
-                                                                        phrase![
-                                                                            Cons(
-                                                                                Box::new(Raw(Text("name".into()))), 
-                                                                                phrase![Raw(Text("John Wick".into()))]
-                                                                            )
-                                                                        ]
-                                                                    ],
-                                                                    verse![
-                                                                        phrase![
-                                                                            Cons(
-                                                                                Box::new(Raw(Text("age".into()))), 
-                                                                                phrase![Raw(Integer(42))]
-                                                                            )
-                                                                        ]
-                                                                    ],
-                                                                    verse![
-                                                                        phrase![
-                                                                            Cons(
-                                                                                Box::new(Raw(Text("dogOwner".into()))), 
-                                                                                phrase![Raw(Boolean(true))]
-                                                                            )
-                                                                        ]
-                                                                    ],
-                                                                ])
-                                                            ]
-                                                        )
-                                                    ]
-                                                ]
-                                            ]
-                                        )
-                                    ]
-                                ],
-                                verse![
-                                    phrase![
-                                        List(
-                                            vec![
-                                                verse![
-                                                    phrase![
-                                                        Cons(
-                                                            Box::new(Raw(Text("id".into()))), 
-                                                            phrase![Raw(Integer(2))]
-                                                        )
-                                                    ]
-                                                ],
-                                                verse![
-                                                    phrase![
-                                                        Cons(
-                                                            Box::new(Raw(Text("details".into()))), 
-                                                            phrase![
-                                                                List(vec![
-                                                                    verse![
-                                                                        phrase![
-                                                                            Cons(
-                                                                                Box::new(Raw(Text("name".into()))), 
-                                                                                phrase![Raw(Text("Max Payne".into()))]
-                                                                            )
-                                                                        ]
-                                                                    ],
-                                                                    verse![
-                                                                        phrase![
-                                                                            Cons(
-                                                                                Box::new(Raw(Text("age".into()))), 
-                                                                                phrase![Raw(Integer(39))]
-                                                                            )
-                                                                        ]
-                                                                    ],
-                                                                    verse![
-                                                                        phrase![
-                                                                            Cons(
-                                                                                Box::new(Raw(Text("dogOwner".into()))), 
-                                                                                phrase![Raw(Boolean(false))]
-                                                                            )
-                                                                        ]
-                                                                    ],
-                                                                ])
-                                                            ]
-                                                        )
-                                                    ]
-                                                ]
-                                            ]
-                                        )
-                                    ]
-                                ]
-                            ])]
-                        )
-                    ]
-                ],
-            ])
-        ]
-    ], verse);
+    assert_eq!(root(
+        object()
+            .key("key1").value(string("value1"))
+            .key("key2").value(integer(1234))
+            .key("key3").value(decimal(1234, 5678, 4))
+            .key("key4").value(negative(integer(345)))
+            .key("key5").value(boolean(true))
+            .key("key6").value(null())
+            .key("emptyArray").value(array())
+            .key("employees").value(array()
+                .with(object()
+                    .key("id").value(integer(1))
+                    .key("details").value(object()
+                        .key("name").value(string("John Wick"))
+                        .key("age").value(integer(42))
+                        .key("dogOwner").value(boolean(true))
+                    )
+                )
+                .with(object()
+                    .key("id").value(integer(2))
+                    .key("details").value(object()
+                        .key("name").value(string("Max Payne"))
+                        .key("age").value(integer(39))
+                        .key("dogOwner").value(boolean(false))
+                    )
+                )
+            )
+    ), verse);
 }
