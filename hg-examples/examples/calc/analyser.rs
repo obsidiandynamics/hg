8150fde43415c78e6d30b171b0458659a34eaa37
Emil Koutanov <ekoutanov@gmail.com>
2025-06-20 14:13:16 +1000

    DynEval refactored into enum

--- hg-examples/examples/calc/analyser.rs
+++ hg-examples/examples/calc/analyser.rs
@@ -1,4 +1,4 @@
-use crate::ast::{DynEval, Number, Product};
+use crate::ast::{EvalKind, Number, Product};
 use hg::metadata::Metadata;
 use hg::token::{Ascii, Token};
 use hg::tree::{Node, Verse};
@@ -21,17 +21,17 @@ pub enum Error {
     StrayExpression(Metadata),
 
     #[error("no expression")]
-    NoExpression
+    NoExpression,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Element {
-    Expr(DynEval, Metadata),
-    Symbol(Ascii, Metadata)
+    Expr(EvalKind, Metadata),
+    Symbol(Ascii, Metadata),
 }
 
 impl Element {
-    fn into_expr(self) -> Option<(DynEval, Metadata)> {
+    fn into_expr(self) -> Option<(EvalKind, Metadata)> {
         match self {
             Element::Expr(eval, metadata) => Some((eval, metadata)),
             Element::Symbol(_, _) => None,
@@ -41,12 +41,12 @@ impl Element {
     fn into_symbol(self) -> Option<(Ascii, Metadata)> {
         match self {
             Element::Expr(_, _) => None,
-            Element::Symbol(ascii, metadata) => Some((ascii, metadata))
+            Element::Symbol(ascii, metadata) => Some((ascii, metadata)),
         }
     }
 }
 
-pub fn analyse(verse: Verse) -> Result<DynEval, Error> {
+pub fn analyse(verse: Verse) -> Result<EvalKind, Error> {
     let node_iter = flatten([verse]);
     let elements = process_elements(node_iter);
     let root = fold_elements(elements)?;
@@ -57,7 +57,9 @@ fn flatten<'a, I: IntoIterator<Item = Verse<'a>>>(into_iter: I) -> impl Iterator
     into_iter.into_iter().flat_map(Verse::flatten)
 }
 
-fn process_elements<'a, I: Iterator<Item = Node<'a>>>(iter: I) -> impl Iterator<Item = Result<Element, Error>> {
+fn process_elements<'a, I: Iterator<Item = Node<'a>>>(
+    iter: I,
+) -> impl Iterator<Item = Result<Element, Error>> {
     iter.map(|node| {
         let element = match node {
             Node::Raw(Token::Integer(uint), metadata) => {
@@ -65,92 +67,82 @@ fn process_elements<'a, I: Iterator<Item = Node<'a>>>(iter: I) -> impl Iterator<
                 Element::Expr(eval, metadata)
             }
             Node::Raw(Token::Decimal(decimal), metadata) => {
-                Element::Expr(Box::new(Number::Float(f64::from(decimal))), metadata)
+                Element::Expr(EvalKind::from(Number::Float(f64::from(decimal))), metadata)
             }
-            Node::Raw(Token::Symbol(Ascii(byte)), metadata) => {
-                match byte {
-                    b'+' => Element::Symbol(Ascii(byte), metadata),
-                    _ => Err(Error::UnexpectedSymbol(Ascii(byte), metadata))?
-                }
-            }
+            Node::Raw(Token::Symbol(Ascii(byte)), metadata) => match byte {
+                b'+' => Element::Symbol(Ascii(byte), metadata),
+                _ => Err(Error::UnexpectedSymbol(Ascii(byte), metadata))?,
+            },
             Node::List(verses, metadata) => {
                 let list_elements = process_elements(flatten(verses));
                 let folded_list = fold_elements(list_elements)?;
                 Element::Expr(folded_list, metadata)
             }
-            Node::Prefix(_, _, _) => {todo!()}
-            other => {
-                Err(Error::UnexpectedNode(other.metadata().clone()))?
+            Node::Prefix(_, _, _) => {
+                todo!()
             }
+            other => Err(Error::UnexpectedNode(other.metadata().clone()))?,
         };
         Ok(element)
     })
 }
 
-fn fold_elements<I: Iterator<Item = Result<Element, Error>>>(iter: I) -> Result<DynEval, Error> {
+fn fold_elements<I: Iterator<Item = Result<Element, Error>>>(iter: I) -> Result<EvalKind, Error> {
     let sans_products = fold_products(iter)?;
 
     // should be no symbols remaining, just one top-level expression
     let mut iter = sans_products.into_iter();
     match iter.next() {
-        None => {
-            Err(Error::NoExpression)
-        }
-        Some(Element::Expr(eval, _)) => {
-            match iter.next() {
-                None => Ok(eval),
-                Some(element) => {
-                    let (ascii, metadata) = element.into_symbol().unwrap();
-                    Err(Error::StrayInfixOperator(ascii, metadata))
-                }
+        None => Err(Error::NoExpression),
+        Some(Element::Expr(eval, _)) => match iter.next() {
+            None => Ok(eval),
+            Some(element) => {
+                let (ascii, metadata) = element.into_symbol().unwrap();
+                Err(Error::StrayInfixOperator(ascii, metadata))
             }
-        }
-        Some(Element::Symbol(ascii, metadata)) => {
-            Err(Error::StrayInfixOperator(ascii, metadata))
-        }
+        },
+        Some(Element::Symbol(ascii, metadata)) => Err(Error::StrayInfixOperator(ascii, metadata)),
     }
 }
 
-fn fold_products<I: Iterator<Item = Result<Element, Error>>>(iter: I) -> Result<Vec<Element>, Error> {
+fn fold_products<I: Iterator<Item = Result<Element, Error>>>(
+    iter: I,
+) -> Result<Vec<Element>, Error> {
     let mut refined = vec![];
     for result in iter {
-        println!("refined: {refined:?}");
+        println!("refined: {refined:#?}");
         let element = result?;
         match element {
-            Element::Expr(eval, metadata) => {
-                match take_last(&mut refined) {
-                    None => {
-                        refined.push(Element::Expr(eval, metadata));
-                    }
-                    Some(Element::Expr(_, _)) => {
-                        return Err(Error::StrayExpression(metadata))
-                    }
-                    Some(Element::Symbol(Ascii(b'*'), _)) => {
-                        let before_last = take_last(&mut refined).unwrap();
-                        let (lhs_eval, lhs_metadata) = before_last.into_expr().unwrap();
-                        let product = Product(vec![lhs_eval, eval]);
-                        refined.push(Element::Expr(Box::new(product), Metadata {
+            Element::Expr(eval, metadata) => match take_last(&mut refined) {
+                None => {
+                    refined.push(Element::Expr(eval, metadata));
+                }
+                Some(Element::Expr(_, _)) => return Err(Error::StrayExpression(metadata)),
+                Some(Element::Symbol(Ascii(b'*'), _)) => {
+                    let before_last = take_last(&mut refined).unwrap();
+                    let (lhs_eval, lhs_metadata) = before_last.into_expr().unwrap();
+                    let product = Product(Box::new(lhs_eval), Box::new(eval));
+                    refined.push(Element::Expr(
+                        EvalKind::from(product),
+                        Metadata {
                             start: lhs_metadata.start,
                             end: metadata.end,
-                        }));
-                    }
-                    Some(other @ Element::Symbol(_, _)) => {
-                        refined.push(other)
-                    }
+                        },
+                    ));
                 }
-            }
-            Element::Symbol(ascii, metadata) => {
-                match take_last(&mut refined) {
-                    None => {
-                        return Err(Error::StrayInfixOperator(ascii, metadata))
-                    }
-                    Some(last @ Element::Expr(_, _)) => {
-                        refined.push(last);
-                        refined.push(Element::Symbol(ascii, metadata));
-                    }
-                    Some(Element::Symbol(_, _)) => {
-                        return Err(Error::StrayInfixOperator(ascii, metadata))
-                    }
+                Some(other @ Element::Symbol(_, _)) => {
+                    refined.push(other);
+                    refined.push(Element::Expr(eval, metadata));
+                }
+            },
+            Element::Symbol(ascii, metadata) => match take_last(&mut refined) {
+                None => return Err(Error::StrayInfixOperator(ascii, metadata)),
+                Some(last @ Element::Expr(_, _)) => {
+                    refined.push(last);
+                    refined.push(Element::Symbol(ascii, metadata));
+                }
+                Some(Element::Symbol(_, _)) => {
+                    return Err(Error::StrayInfixOperator(ascii, metadata));
                 }
             },
         }
@@ -167,65 +159,19 @@ fn take_last<T>(vec: &mut Vec<T>) -> Option<T> {
     }
 }
 
-// pub fn analyse_elements<'a>(elements: Vec<Element<'a>>) -> Result<DynEval, Error> {
-//     let mut old_elements = elements;
-//     let mut new_elements = vec![];
-//     loop {
-//         let mut has_pending = false;
-//         for current in old_elements {
-//             let analysed = match current {
-//                 Element::Expr(eval) => eval,
-//                 Element::Pending(pending) => {
-//                     has_pending = true;
-//                     match pending {
-//                         Node::Raw(Token::Integer(uint), metadata) => {
-//                             convert_integer(uint, metadata)?
-//                         }
-//                         Node::Raw(Token::Decimal(decimal), _) => {
-//                             Box::new(Number::Float(f64::from(decimal)))
-//                         }
-//                         Node::Raw(Token::Symbol(Ascii(b'+')), metadata) => {
-//                             todo!()
-//                         }
-//                         Node::List(verses, _) => {
-//                             analyse_elements(flatten(verses).collect())?
-//                         }
-//                         Node::Prefix(_, _, _) => {todo!()}
-//                         other => {
-//                             Err(Error::UnexpectedNode(other.metadata().clone()))?
-//                         }
-//                     }
-//                 }
-//             };
-//             new_elements.push(Element::Expr(analysed));
-//         }
-//
-//         old_elements = mem::take(&mut new_elements);
-//         if !has_pending {
-//             break;
-//         }
-//     }
-//     assert_eq!(1, old_elements.len());
-//     todo!()
-// }
-
-fn convert_integer(uint: u128, metadata: Metadata) -> Result<(DynEval, Metadata), Error> {
+fn convert_integer(uint: u128, metadata: Metadata) -> Result<(EvalKind, Metadata), Error> {
     match i64::try_from(uint) {
-        Ok(int) => {
-            Ok((Box::new(Number::Integer(int)), metadata))
-        }
-        Err(_) => {
-            Err(Error::InvalidInteger(uint, metadata))?
-        }
+        Ok(int) => Ok((EvalKind::from(Number::Integer(int)), metadata)),
+        Err(_) => Err(Error::InvalidInteger(uint, metadata))?,
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::analyser::{Element, Error, fold_products, take_last};
+    use crate::ast::{Eval, EvalKind, Number, Product, Sum};
     use hg::metadata::Metadata;
     use hg::token::Ascii;
-    use crate::analyser::{analyse, fold_products, take_last, Element, Error};
-    use crate::ast::{DynEval, Number};
 
     #[test]
     fn vec_take_last() {
@@ -234,10 +180,13 @@ mod tests {
         assert!(vec.is_empty());
         assert_eq!(None, take_last(&mut vec));
     }
-    
+
     impl From<i32> for Element {
         fn from(int: i32) -> Self {
-            Element::Expr(Box::new(Number::Integer(int as i64)), Metadata::unspecified())
+            Element::Expr(
+                EvalKind::from(Number::Integer(int as i64)),
+                Metadata::unspecified(),
+            )
         }
     }
 
@@ -246,30 +195,101 @@ mod tests {
             Element::Symbol(Ascii(byte), Metadata::unspecified())
         }
     }
-    
-    fn sans_error<I: IntoIterator<Item = Element>>(into_iter: I) -> impl Iterator<Item = Result<Element, Error>> {
+
+    fn sans_error<I: IntoIterator<Item = Element>>(
+        into_iter: I,
+    ) -> impl Iterator<Item = Result<Element, Error>> {
         into_iter.into_iter().map(Ok)
     }
-    
-    fn fold_products_ok<I: IntoIterator<Item = Element>>(into_iter: I) -> DynEval {
-        let folded = fold_products(sans_error(into_iter)).unwrap();
-        assert_eq!(1, folded.len());
-        let (expr, _) = folded.into_iter().next().unwrap().into_expr().unwrap();
-        expr
+
+    fn fold_products_ok<I: IntoIterator<Item = Element>>(into_iter: I) -> Vec<Element> {
+        fold_products(sans_error(into_iter)).unwrap()
     }
-    
+
     #[test]
     fn fold_products_2() {
         // let elements = vec![Element::Expr(Box::new(Number::Integer(5)), Metadata::unspecified()), Element::Symbol(Ascii(b'*'), Metadata::unspecified()), Element::Expr(Box::new(Number::Integer(7)), Metadata::unspecified())];
         let elements = [Element::from(3), Element::from(b'*'), Element::from(4)];
-        let expr = fold_products_ok(elements);
+        let folded = fold_products_ok(elements);
+        assert_eq!(1, folded.len());
+        let (expr, _) = folded.into_iter().next().unwrap().into_expr().unwrap();
         assert_eq!(12.0, expr.eval());
     }
 
     #[test]
     fn fold_products_3() {
-        let elements = [Element::from(3), Element::from(b'*'), Element::from(4), Element::from(b'*'), Element::from(5)];
-        let expr = fold_products_ok(elements);
+        let elements = [
+            Element::from(3),
+            Element::from(b'*'),
+            Element::from(4),
+            Element::from(b'*'),
+            Element::from(5),
+        ];
+        let folded = fold_products_ok(elements);
+        assert_eq!(1, folded.len());
+        let (expr, _) = folded.into_iter().next().unwrap().into_expr().unwrap();
         assert_eq!(60.0, expr.eval());
+    }
+
+    #[test]
+    fn fold_products_with_trailing_sum() {
+        let elements = [
+            Element::from(3),
+            Element::from(b'*'),
+            Element::from(4),
+            Element::from(b'+'),
+            Element::from(5),
+        ];
+        let folded = fold_products_ok(elements);
+        assert_eq!(3, folded.len());
+        assert_eq!(
+            vec![
+                Element::Expr(
+                    EvalKind::from(Product(
+                        Box::from(EvalKind::from(Number::Integer(3))),
+                        Box::from(EvalKind::from(Number::Integer(4)))
+                    )),
+                    Metadata::unspecified()
+                ),
+                Element::Symbol(Ascii(b'+'), Metadata::unspecified()),
+                Element::Expr(EvalKind::from(Number::Integer(5)), Metadata::unspecified())
+            ],
+            folded
+        );
+    }
+
+    #[test]
+    fn fold_products_with_mid_sum() {
+        let elements = [
+            Element::from(3),
+            Element::from(b'*'),
+            Element::from(4),
+            Element::from(b'+'),
+            Element::from(5),
+            Element::from(b'*'),
+            Element::from(6),
+        ];
+        let folded = fold_products_ok(elements);
+        assert_eq!(3, folded.len());
+        assert_eq!(
+            vec![
+                Element::Expr(
+                    EvalKind::from(Product(
+                        Box::from(EvalKind::from(Number::Integer(3))),
+                        Box::from(EvalKind::from(Number::Integer(4)))
+                    )),
+                    Metadata::unspecified()
+                ),
+                Element::Symbol(Ascii(b'+'), Metadata::unspecified()),
+                Element::Expr(
+                    EvalKind::from(Product(
+                        Box::from(EvalKind::from(Number::Integer(5))),
+                        Box::from(EvalKind::from(Number::Integer(6)))
+                    )),
+                    Metadata::unspecified()
+                ),
+            ],
+            folded
+        );
     }
 }
--- hg-examples/examples/calc/ast.rs
+++ hg-examples/examples/calc/ast.rs
@@ -4,10 +4,43 @@ pub trait Eval: Debug {
     fn eval(&self) -> f64;
 }
 
-pub type DynEval = Box<dyn Eval>;
+#[derive(Debug, PartialEq)]
+pub enum EvalKind {
+    Sum(Sum),
+    Product(Product),
+    Number(Number)
+}
 
-#[derive(Debug)]
-pub struct Sum(pub DynEval, pub DynEval);
+impl Eval for EvalKind {
+    fn eval(&self) -> f64 {
+        match self {
+            EvalKind::Sum(sum) => sum.eval(),
+            EvalKind::Product(product) => product.eval(),
+            EvalKind::Number(number) => number.eval()
+        }
+    }
+}
+
+impl From<Sum> for EvalKind {
+    fn from(value: Sum) -> Self {
+        EvalKind::Sum(value)
+    }
+}
+
+impl From<Product> for EvalKind {
+    fn from(value: Product) -> Self {
+        EvalKind::Product(value)
+    }
+}
+
+impl From<Number> for EvalKind {
+    fn from(value: Number) -> Self {
+        EvalKind::Number(value)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Sum(pub Box<EvalKind>, pub Box<EvalKind>);
 
 impl Eval for Sum {
     fn eval(&self) -> f64 {
@@ -15,16 +48,16 @@ impl Eval for Sum {
     }
 }
 
-#[derive(Debug)]
-pub struct Product(pub Vec<DynEval>);
+#[derive(Debug, PartialEq)]
+pub struct Product(pub Box<EvalKind>, pub Box<EvalKind>);
 
 impl Eval for Product {
     fn eval(&self) -> f64 {
-        self.0.iter().map(|eval| eval.eval()).product()
+        &self.0.eval() * &self.1.eval()
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Number {
     Integer(i64),
     Float(f64)
